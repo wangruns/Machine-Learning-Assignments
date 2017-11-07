536a670f1026ec4723013ffbfc402f07a56e9484
wangruns <1344886192@qq.com>
2017-11-07 19:52:53 +0800

    machine-learning-ex7 is finished

---
+++ machine-learning-ex7/ex7/pca.m
@@ -0,0 +1,32 @@
+function [U, S] = pca(X)
+%PCA Run principal component analysis on the dataset X
+%   [U, S, X] = pca(X) computes eigenvectors of the covariance matrix of X
+%   Returns the eigenvectors U, the eigenvalues (on diagonal) in S
+%
+
+% Useful values
+[m, n] = size(X);
+
+% You need to return the following variables correctly.
+U = zeros(n);
+S = zeros(n);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: You should first compute the covariance matrix. Then, you
+%               should use the "svd" function to compute the eigenvectors
+%               and eigenvalues of the covariance matrix. 
+%
+% Note: When computing the covariance matrix, remember to divide by m (the
+%       number of examples).
+%
+
+
+%compute the covariance matrix
+Sigma=X'*X/m;
+%call the build-in function SVD to compute principal components
+[U,S,V]=svd(Sigma);
+
+
+% =========================================================================
+
+end
